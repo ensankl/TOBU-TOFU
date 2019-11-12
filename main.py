f69372f62198eff1b00fe51d42a065c6cd396c3c
ensankl <necrophilia34564@gmail.com>
2019-11-12 15:58:38 +0900

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+##import other files
+import sys
+import readchar
+
+def if __name__ == "__main__":
+    pass
+    ##render()
+    ##sensing()
+    ##inputKey() : Using readchar.readkey()
+    ##
---
+++ sensor.py
@@ -0,0 +1,3 @@
+##import GPIO and so on
+
+##copy test.py to this file
